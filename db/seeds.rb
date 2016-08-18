a757d781ea3c70a842decb25834c24f6254df761
rmeoni <rodrigo.me13@gmail.com>
2016-08-18 00:14:11 -0400

    adding templates and re styling home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	template1 = Template.create(name: 'Nakamarra', image: '/images/Nakamarra-thumb.jpg', description: 'A template designed for a restaurant, bar, or a food store.', url: "https://nakamarra.herokuapp.com", category: 'restaurant')
+				Template.create(name: 'Rodrigo Manuel', image: '/images/rodrigomanuel-thumb.jpg', description: 'A template designed for a photograph portfolio', url: "https://rodrigomanuel.herokuapp.com", category: 'photography')
+				Template.create(name: 'San Telmo', image: '/images/santelmo-thumb.jpg', description: 'A template designed for a restaurant or bar', url: "https://santelmo.herokuapp.com", category: 'restaurant')
+
