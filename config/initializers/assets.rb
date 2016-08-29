1aa6906d82c89e2be6ebb11125cb711ea2f9f92b
rmeoni <rodrigo.me13@gmail.com>
2016-08-28 22:35:12 -0400

    re styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( jquery.fullPage.min.js )
 Rails.application.config.assets.precompile += %w( jquery.fullPage.css )
 Rails.application.config.assets.precompile += %w( examples.css )
 Rails.application.config.assets.precompile += %w( examples.js )
+Rails.application.config.assets.precompile += %w( jquery.slimscroll.js ) 
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
